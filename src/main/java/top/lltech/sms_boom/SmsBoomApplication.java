32eb22456cd5bcf6fdfc9172d38bfdb9239b7f8c
qihl <979529235@qq.com>
2024-12-18 09:10:39 +0800

    first commit

---
+++ src/main/java/top/lltech/sms_boom/SmsBoomApplication.java
@@ -0,0 +1,13 @@
+package top.lltech.sms_boom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsBoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsBoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/lltech/sms_boom/SmsBoomApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lltech.sms_boom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsBoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
