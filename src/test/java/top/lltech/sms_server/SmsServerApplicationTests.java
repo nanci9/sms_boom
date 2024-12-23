50b4bb37e371b8b5d7ab241a35282d716dfca000
qihl <979529235@qq.com>
2024-12-23 21:53:40 +0800

    t

---
+++ src/test/java/top/lltech/sms_server/SmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lltech.sms_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
